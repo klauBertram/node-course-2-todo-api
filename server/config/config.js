0e7c04f85c41ddbdf230448d3cd10dd32cc5ab31
klauBertram <klau@bcap.com>
2017-02-04 19:21:15 -0800

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -4,11 +4,23 @@ if(env !== 'production'){
   console.log('env:', env);
 }
 
+if(env === 'development' || env === 'test'){
+  var config = require('./config.json');
+  // console.log(config);
+  var envConfig = config[env];
 
-if(env == 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if(env === 'test'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+  console.log('keys', Object.keys(envConfig));
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+
+// if(env == 'development'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if(env === 'test'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
--- server/models/user.js
+++ server/models/user.js
@@ -56,7 +56,7 @@ UserSchema.methods.generateAuthToken = function(){
   var token = jwt.sign({
     _id: user._id.toHexString(),
     access
-  }, 'abc123').toString();
+  }, process.env.JWT_SECRET).toString();
 
   user.tokens.push({
     access,
@@ -107,7 +107,7 @@ UserSchema.statics.findByToken = function(token){
   var decoded;
 
   try{
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
     // console.log('decoded:', decoded);
   } catch(error){
     // return new Promise((resolve, reject) => {
